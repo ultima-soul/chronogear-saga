7d70857a28e607007013f4e3e7031fffbe6b37c2
Akshay Jhanji <akshayjhanji@hotmail.com>
2023-04-20 10:37:39 -0700

    Fix Camera to instantly snap to limits at start of level

--- src/Player/camera.gd
+++ src/Player/camera.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 
 	var rectangle: Rect2 = level.get_used_rect()
 	limit_left = (rectangle.position.x + 1) * level.cell_size.x
-	limit_right = rectangle.end.x * level.cell_size.x
+	limit_right = (rectangle.end.x - 1) * level.cell_size.x
 	limit_top = rectangle.position.y * level.cell_size.y
 	limit_bottom = (rectangle.end.y - 1) * level.cell_size.y
+	reset_smoothing()
